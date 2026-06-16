2790995a13af2f9180c90583c6b57a189afe430e
Dio-79 <umwenijesse36@gmail.com>
2026-06-16 13:37:54 -0600

    commit

--- indego/src/app/index.tsx
+++ indego/src/app/index.tsx
@@ -4,23 +4,26 @@ import { Text, View, StyleSheet, Button, Alert, ScrollView, Image } from "react-
 
 export default function Index() {
   return (
-    
+    <View style={styles.wrapper}>
+
     <View style={styles.container}>
 
 
     <View style={styles.Header}>
  
-  <Text style={{paddingRight:3,color:"#FFFF"}}> Events|</Text>
-  <Text style={{paddingRight:3,color:"#FFFF"}}> Gift Cards| </Text>
-  <Text style={{paddingRight:3,color:"#FFFF"}}> Plum Rewards| </Text>
-  <Text style={{paddingRight:3,color:"#FFFF"}}> Order Status| </Text>
-   <Text style={{paddingRight:3,color:"#FFFF"}}>|Français </Text>
+  <Text style={{paddingRight:3,color:"#ffffff"}}> Events |</Text>
+  <Text style={{paddingRight:3,color:"#ffffff"}}> Gift Cards | </Text>
+  <Text style={{paddingRight:3,color:"#ffffff"}}> Plum Rewards | </Text>
+  <Text style={{paddingRight:3,color:"#ffffff"}}> Order Status </Text>
+   <Text style={{paddingRight:3,color:"#ffffff"}}>| Français </Text>
+ </View>
+</View>
 
-    </View>
    
   <View style={styles.navBar}>
-  <Text style={{color:"#0000"}}>Indego</Text>
-
+  <View style={styles.title}>
+  <Text style={{color:"#000000",fontSize:23}}>Indego</Text>
+</View>
   </View>
     </View>
 
@@ -31,8 +34,9 @@ export default function Index() {
 
 
   const styles = StyleSheet.create({
+    wrapper:{},
 
-    container: { marginBottom:1,
+    container: { marginBottom:15,
                 borderWidth:1,
                 
                  
@@ -44,12 +48,19 @@ export default function Index() {
     Header:{ 
       flexDirection:"row",
       justifyContent:"flex-end",
-      backgroundColor:"#0000"
+      backgroundColor:"#000000"
 
 
     },
     navBar:{
 
+
+    },
+
+    title:{paddingTop:13,
+      
+          
+           
 
     }
   
